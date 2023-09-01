63f6ce47c4800c139fd809875e6a9c106bdfa7b5
Lilglu4e <mohammadsarrafmstr2000@gmail.com>
2023-09-01 18:57:42 -0400

    netlify update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,237 @@
-import Image from 'next/image'
+'use client'
+import React, { useState, useEffect } from "react";
+import Board from './Components/Board'
+import ScoreBoard from "./Components/ScoreBoard";
+import Dice from './Components/Dice'
+import { initializeApp } from "firebase/app";
+import { getFirestore, updateDoc } from "firebase/firestore";
+import { doc, setDoc, onSnapshot, getDoc } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import Options from "./Components/Options";
+import * as cotl from './Functions/Class';
+import * as fb from './Functions/firebase'
+import './Components/App.css';
+import './Components/GameOver.css'
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const [playerOne, setPlayerOneBoard] = useState(Array(9).fill(null))
+	const [playerTwo, setPlayerTwoBoard] = useState(Array(9).fill(null))
+	const [sessionID, setSessionID] = useState("000000");
+	const [playerXPlaying, setPlayerxPlayer] = useState(true)
+	const [die, setDie] = useState(0)
+	const [userName, setUserName] = useState("0")
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	// const app = fb.app
+	// const analytics = fb.analytics
+	const db = fb.db
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const AlertSession = () => {
+		const input: string | null = prompt("Please enter the session");
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+		if (input === null) {
+			// The user canceled the prompt
+			alert("Session ID input canceled.");
+			return;
+		}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+		if (/^\d{6}$/.test(input)) {
+			const ID: string = input; // Assign the input as is, since it's a 6-digit string
+			setSessionID(ID);
+			localStorage.setItem("userName", "2");
+			setUserName("2");
+		} else {
+			alert("Invalid session ID. Please enter a valid session ID.");
+		}
+	};
+
+
+
+	useEffect(() => {
+
+		localStorage.setItem("userName", "1")
+		setUserName("1")
+
+	}, [])
+
+
+	useEffect(() => {
+
+		rotateDice()
+
+	}, [die])
+
+
+	useEffect(() => {
+		const joinSession = async () => {
+			let diemove = Math.floor(Math.random() * 6 + 1);
+			fb.docSnap(sessionID, playerOne, setPlayerOneBoard, playerTwo, setPlayerTwoBoard, diemove, setDie, setPlayerxPlayer)
+		}
+
+		joinSession();
+	}, []);
+
+
+
+	useEffect(() => {
+		const unsub = onSnapshot(doc(db, "Sessions", sessionID), (doc: any) => {
+
+			setPlayerOneBoard(cotl.sort(doc.data().playerone))
+			setPlayerTwoBoard(cotl.sort(doc.data().playertwo))
+			setDie(doc.data().die)
+			setPlayerxPlayer(doc.data().playerXPlaying)
+
+		})
+
+
+	}, [sessionID])
+
+	const handleBoxClickPlayerOne = (indx: number) => {
+
+		if (localStorage.getItem("userName") === "2") {
+			return; // If playerTwo is stored in local storage, do nothing and return
+		}
+
+		const updateBoard = playerOne.map((value, index) => {
+			if (index === indx && playerXPlaying === true) {
+
+				cotl.updateChange(die, index, playerTwo);
+				return die;
+			} else {
+				return value;
+			}
+		})
+
+		if (playerXPlaying) {
+			let diemove = Math.floor(Math.random() * 6 + 1);
+			const Ref = doc(db, "Sessions", sessionID);
+			updateDoc(Ref, {
+				playerone: updateBoard,
+				playertwo: playerTwo,
+				playerXPlaying: false,
+				die: diemove,
+			});
+
+		}
+
+	}
+
+	const handleBoxClickPlayerTwo = (indx: number) => {
+
+		if (localStorage.getItem("userName") === "1") {
+			return; // If playerTwo is stored in local storage, do nothing and return
+		}
+
+		const updateBoard = playerTwo.map((value, index) => {
+			if (index === indx && playerXPlaying === false) {
+				cotl.updateChange(die, index, playerOne)
+
+				return die;
+			} else {
+				return value;
+			}
+		})
+
+		if (!playerXPlaying) {
+
+			const Ref = doc(db, "Sessions", sessionID);
+			let diemove = Math.floor(Math.random() * 6 + 1);
+
+			updateDoc(Ref, {
+				playerone: playerOne,
+				playertwo: updateBoard,
+				playerXPlaying: true,
+				die: diemove
+			});
+
+		}
+
+	}
+
+	const resetBoard = () => {
+		rotateDice();
+		const Ref = doc(db, "Sessions", sessionID);
+		let diemove = Math.floor(Math.random() * 6 + 1);
+
+		updateDoc(Ref, {
+			playerone: Array(9).fill(null),
+			playertwo: Array(9).fill(null),
+			die: diemove,
+			playerXPlaying: true
+
+		});
+	}
+
+	const rotateDice = () => {
+		const styles: CSSStyleDeclaration = getComputedStyle(document.body);
+
+		// This section is only to make sure you select the whole dice
+		const diceElement = document.getElementsByClassName("dice")[0] as HTMLElement | null;
+
+		if (diceElement) {
+			diceElement.classList.toggle('random-rotation');
+			setTimeout(function () {
+				diceElement.classList.remove('random-rotation');
+			}, 1500);
+
+			// Targeted side
+			var facingSide = die;
+			var transform = null;
+
+			switch (facingSide) {
+				case 1:
+					transform = styles.getPropertyValue('--dice-face-one');
+					break;
+				case 2:
+					transform = styles.getPropertyValue('--dice-face-two');
+					break;
+				case 3:
+					transform = styles.getPropertyValue('--dice-face-three');
+					break;
+				case 4:
+					transform = styles.getPropertyValue('--dice-face-four');
+					break;
+				case 5:
+					transform = styles.getPropertyValue('--dice-face-five');
+					break;
+				case 6:
+					transform = styles.getPropertyValue('--dice-face-six');
+					break;
+				default:
+					transform = null;
+			}
+
+			if (transform !== null) {
+				diceElement.style.transform = transform;
+				diceElement.style.transition = "all 0.1s ease-out";
+			}
+		}
+	}
+
+	return (
+		<>
+			{cotl.handleGameOver(playerOne, playerTwo) ? (
+				<section className="gameover-section">
+					<section className="gameover-container">
+						<h1 className="game-decision">{cotl.winner(playerOne, playerTwo)}</h1>
+						<section className="button-section">
+							<button onClick={resetBoard}>Reset Game</button>
+							<button onClick={AlertSession}>Join Game</button>
+						</section>
+					</section>
+				</section>
+
+			) : (<></>)}
+			<div className="Game">
+				<ScoreBoard names={{ playerOneName: "POne", playerTwoName: "PTwo" }} scores={cotl.updateScore(playerOne, playerTwo)} playerXPlaying={playerXPlaying} ID={sessionID} />
+
+				<Board name={"X"} board={playerOne} onClick={handleBoxClickPlayerOne} die={die} isPlaying={playerXPlaying} />
+				<Dice rotateDice={rotateDice} />
+				<Board name={"O"} board={playerTwo} onClick={handleBoxClickPlayerTwo} die={die} isPlaying={playerXPlaying} />
+			</div>
+			<Options resetGame={resetBoard} joinGame={AlertSession} />
+		</>
+
+	)
 }
